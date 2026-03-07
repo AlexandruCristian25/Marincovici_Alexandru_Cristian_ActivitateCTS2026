0f1efc9d397121ed2200b06a597e4428d5425fc7
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-07 21:52:19 +0200

    Add files via upload

---
+++ BlocSingleton/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,20 @@
+package ro.ase.cts.Main;
+
+import ro.ase.cts.Bloc.Bloc;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Bloc b1 = Bloc.getInstance();
+        Bloc b2 = Bloc.getInstance();
+        
+        b1.setNumeBloc("Nume bloc: M2/4");
+        b1.setNrApartamente("Nr apartamente: 78");
+        
+        System.out.println(b1.getNumeBloc());
+        System.out.println(b1.getNrApartamente());
+        
+    }
+    
+}
