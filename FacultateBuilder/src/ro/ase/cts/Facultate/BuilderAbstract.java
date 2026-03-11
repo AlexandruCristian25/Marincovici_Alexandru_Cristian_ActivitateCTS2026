e65d10506a3c9f23164bcd248a580801b5d0cccb
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-11 11:58:12 +0200

    Add files via upload

---
+++ FacultateBuilder/src/ro/ase/cts/Facultate/BuilderAbstract.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.Facultate;
+
+public interface BuilderAbstract {
+    
+    BuilderAbstract.setNume(String nume);
+    BuilderAbstract.setLocatie(String locatie);
+    BuilderAbstract.setSpecializare(String specializare);
+    Facultate buid();
+    
+}
---
+++ FacultateBuilder/src/ro/ase/cts/Facultate/Facultate.java
@@ -0,0 +1,69 @@
+package ro.ase.cts.Facultate;
+
+public class Facultate {
+    
+    private String nume;
+    private String locatie;
+    private String specializare;
+    
+    private Facultate(String nume, String locatie, String specializare) {
+        
+        this.nume = nume;
+        this.locatie = locatie;
+        this.specializare = specializare;
+        
+    }
+    
+    public static class Builder implements BuilderAbstract {
+        
+        private String nume;
+        private String locatie;
+        private String specializare;
+        
+        @Override
+        public BuilderAbstract setNume(String nume) {
+            
+            this.nume = nume;
+            return this;
+            
+        }
+        
+        @Override
+        public BuilderAbstract setLocatie(String locatie) {
+            
+            this.locatie = locatie;
+            return this;
+            
+        }
+        
+        @Override
+        public BuilderAbstract setSpecializare(String specializare) {
+            
+            this.specializare = specializare;
+            return this;
+            
+        }
+        
+        @Override
+        public Facultate build() {
+            
+            return new Facultate(nume, locatie, specializare);
+            
+        }
+        
+    } 
+    
+    @Override
+    public String toString() {
+        
+        final StringBuilder sb = new StringBuilder("Facultate{");
+        sb.append("nume='").append(nume).append('\'');
+        sb.append(", locatie='").append(locatie).append('\'');
+        sb.append(", specializare='").append(specializare);
+        sb.append('{');
+        
+        return sb.toString();
+        
+    }
+    
+}
