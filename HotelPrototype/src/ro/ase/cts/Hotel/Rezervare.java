02459480ab35602ec42ba21959ce42999c06cd32
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-12 12:07:09 +0200

    Add files via upload

---
+++ HotelPrototype/src/ro/ase/cts/Hotel/Rezervare.java
@@ -0,0 +1,69 @@
+public class Rezervare {
+    
+    private String nume;
+    private String email;
+    private int nrPersoane;
+    
+    private Rezervare() {
+    }
+    
+    public Rezervare(String nume, String email, int nrPersoane) {
+        
+        if(this.nume.length() > 3) {
+            
+            this.nume = nume;
+            
+        } else {
+            
+            this.nume = "NaN";
+            
+        }
+        
+        if(this.email.length() > 3) {
+            
+            this.email = email;
+            
+        } else {
+            
+            this.email = "NaN";
+            
+        }
+        
+        if(this.nrPersoane.length() > 0) {
+            
+            this.nrPersoane = nrPersoane;
+            
+        } else {
+            
+            this.nrPersoane = nrPersoane;
+            
+        }
+        
+    }
+    
+    public void setNrPersoane(int nrPersoane) {
+        
+        this.nrPersoane = nrPersoane;
+        
+    }
+    
+    @Override
+    public String toString() {
+        
+        StringBuilder sb = new StringBuilder("Rezervare{");
+        sb.append("nume='").append(nume).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", nrTelefon='").append(nrTelefon);
+        sb.append('{');
+        
+        return sb.toString();
+        
+    }
+    
+    public Rezervare copiaza() throws CloneNotSupportedException {
+        
+        return (Rezervare) super.clone();
+        
+    }
+    
+}
