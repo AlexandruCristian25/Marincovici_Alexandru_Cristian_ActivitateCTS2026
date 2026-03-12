1824ea6a15f72d618a7a4fde99e417f94358d978
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-12 12:06:32 +0200

    Add files via upload

---
+++ HotelPrototype/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Rezervare rezervare = new Rezervare("Popescu", "popescu@yahoo.com", 3);
+        Rezervare rezervare2 = null;
+        
+        try {
+            
+            rezervare2 = rezervare.copiaza();
+            
+        } catch(CloneNotSupportedException e) {
+            
+            throw new RuntimeException(e);
+            
+        }
+        
+        rezervare2.setNrPersoane(5);
+        
+        System.out.println(rezervare.toString());
+        System.out.println(rezervare2.toString());
+        
+    }
+}
