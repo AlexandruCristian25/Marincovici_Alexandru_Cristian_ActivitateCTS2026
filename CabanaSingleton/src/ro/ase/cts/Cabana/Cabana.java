f3d32a036b2cd4da39571bb063a8bf9d5487eea1
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-16 18:33:47 +0200

    Add files via upload

---
+++ CabanaSingleton/src/ro/ase/cts/Cabana/Cabana.java
@@ -0,0 +1,61 @@
+package ro.ase.cts.Cabana;
+
+public class Cabana {
+    
+    private String locatie;
+    private int nrCamere;
+    private static Cabana instance = null;
+    
+    private Cabana(String locatie, int nrCamere) {
+        
+        this.locatie = locatie;
+        this.nrCamere = nrCamere;
+        
+    }
+    
+    public String getLocatie() {
+        
+        return this.locatie;
+        
+    }
+    
+    public void setLocatie(String locatie) {
+        
+        this.locatie = locatie;
+        
+    }
+    
+    public int getNrCamere() {
+        
+        return this.nrCamere;
+        
+    }
+    
+    public void setNrCamere(int nrCamere) {
+        
+        this.nrCamere = nrCamere;
+        
+    }
+    
+    public static synchronized Cabana getInstance(String locatie, int nrCamere) {
+        
+        if(instance == null) {
+            
+            instance = new Cabana(locatie, nrCamere);
+            
+        }
+        
+        return instance;
+        
+    }
+    
+    publoc void afisareDetaliiCabana() {
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.locatie).append(this.nrCamere);
+        
+        System.out.println(sb);
+        
+    }
+    
+}
