25133a1b4a303c218c47e049398d9807dcd2eb45
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-16 23:04:24 +0200

    Add files via upload

---
+++ AutorSingleton/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.Main;
+
+package ro.ase.cts.Autor.Autor;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Autor a1 = Autor.getInstance();
+        Autor a2 = Autor.getInstance();
+        
+        a2.setNume("Nume: Eminescu");
+        
+        System.out.println(a2.getNume());
+        
+    }
+}
