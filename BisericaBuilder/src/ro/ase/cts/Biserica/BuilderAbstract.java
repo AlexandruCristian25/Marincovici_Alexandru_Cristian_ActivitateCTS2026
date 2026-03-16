4a541abb3c0fe6e5c6db00439bcb26d9a84101be
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-16 18:26:47 +0200

    Add files via upload

---
+++ BisericaBuilder/src/ro/ase/cts/Biserica/BuilderAbstract.java
@@ -0,0 +1,9 @@
+package to.ase.cts.Biserica;
+
+public interface BuilderAbstract {
+    
+    BuilderAbstract.setNume(String nume);
+    BuilderAbstract.setLocatie(String locatie);
+    Biserica build();
+    
+}
