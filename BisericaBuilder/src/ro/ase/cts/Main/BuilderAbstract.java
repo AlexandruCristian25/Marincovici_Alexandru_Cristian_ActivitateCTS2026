2ffed81dc73690f4392ad5a0494c6193db9038a7
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-16 18:27:56 +0200

    Delete BisericaBuilder/src/ro/ase/cts/Main/BuilderAbstract.java

--- BisericaBuilder/src/ro/ase/cts/Main/BuilderAbstract.java
+++
@@ -1,9 +0,0 @@
-package to.ase.cts.Biserica;
-
-public interface BuilderAbstract {
-    
-    BuilderAbstract.setNume(String nume);
-    BuilderAbstract.setLocatie(String locatie);
-    Biserica build();
-    
-}
