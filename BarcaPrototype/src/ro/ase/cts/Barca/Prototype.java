13911a2f09fc5fb261ad2fc925547b530130a9cf
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-17 12:17:09 +0200

    Add files via upload

---
+++ BarcaPrototype/src/ro/ase/cts/Barca/Prototype.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.Barca;
+
+public interface Prototype {
+    
+    Prototype copiaza();
+    
+}
---
+++ BarcaPrototype/src/ro/ase/cts/Barca/RezervareBarca.java
@@ -0,0 +1,67 @@
+package ro.ase.cts.Barca;
+
+public class RezervareBarca implements Cloneable {
+    
+    private String marca;
+    private int nrZile;
+    private String numePersoana;
+    
+    private RezervareBarca() {
+    }
+    
+    public RezervareBarca(String marca, int nrZile, String numePersoana) {
+        
+        if(marca.length() > 3) {
+            
+            this.marca = marca;
+            
+        } else {
+            
+            this.marca = "NaN";
+            
+        }
+        
+        if(nrZile.length() > 1) {
+            
+            this.nrZile = nrZile;
+            
+        } else {
+            
+            this.nrZile = "0";
+            
+        }
+        
+        if(numePersoana.length() == 0) {
+            
+            this.numePersoana = numePersoana;
+            
+        }
+        
+    }
+    
+    private setNumePersoana(String numePersoana) {
+        
+        this.numePersoana = numePersoana;
+        
+    }
+    
+    @Override
+    public String toString() {
+        
+        final StringBuilder sb = new StringBuilder("RezervareBarca{");
+        sb.append("marca='").append(marca).append('\'');
+        sb.append(", nrZile='").append(nrZile).append('\'');
+        sb.append(", numePersoana='").append(numePersoana);
+        sb.append('{');
+        
+        return sb.toString();
+        
+    }
+    
+    public RezervareBarca copiaza() throws CloneNotSupportedException {
+        
+        return new(RezervareBarca) super.clone();
+        
+    }
+    
+}
