db6e79827b082d3dd0a15fd3dbd9d12db915bd89
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-19 12:36:13 +0200

    Update Main.java

--- CarteFactoryMethod/src/ro/ase/cts/Main/Main.java
+++ CarteFactoryMethod/src/ro/ase/cts/Main/Main.java
@@ -5,9 +5,11 @@ import ro.ase.cts.Carte.Carte;
 public class Main {
     public static void main(String[] args) {
         
-        Carte c1 = CaryeFactory.creeazaCarte(TipCarte.CarteStiinta, "Autor1", 230);
-        Carte c2 = CaryeFactory.creeazaCarte(TipCarte.CarteFizica, "Autor2", 160);
-        Carte c3 = CaryeFactory.creeazaCarte(TipCarte.CarteMatematica, "Autor3", 200);
+        CarteFactory carteFactory = new CarteFactory();
+        
+        Carte c1 = carteFactory.creeazaCarte(TipCarte.CarteStiinta, "Autor1", 230);
+        Carte c2 = carteFactory.creeazaCarte(TipCarte.CarteFizica, "Autor2", 160);
+        Carte c3 = carteFactory.creeazaCarte(TipCarte.CarteMatematica, "Autor3", 200);
         
         c1.afiseazaInfo();
         c2.afiseazaInfo();
