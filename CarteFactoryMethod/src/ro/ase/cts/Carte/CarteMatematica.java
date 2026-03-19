b005258799eb7203f0f6132a0b4d8bf8e8c77bb5
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-19 12:34:51 +0200

    Update CarteMatematica.java

--- CarteFactoryMethod/src/ro/ase/cts/Carte/CarteMatematica.java
+++ CarteFactoryMethod/src/ro/ase/cts/Carte/CarteMatematica.java
@@ -11,7 +11,11 @@ public class CarteMatematica {
     @Override
     public void afiseazaInfo() {
         
-        System.out.println("Cartea de matematica cu autorul " + autor + " are " + nrPagini + " pagini");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Auror: ").append(super.autor);
+        sb.append("Nr pagini: ").append(super.nrPagini);
+        
+        System.out.println(sb.toString());
         
     }
     
