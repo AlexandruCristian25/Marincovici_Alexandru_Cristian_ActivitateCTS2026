00c4e27975dce2cc4412e87c0fb2dafec20e5c1c
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-21 07:00:04 -0700

    Add files via upload

---
+++ CuptorSingleton/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.Main;
+
+import ro.ase.cts.Cuptor.Cuptor;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Cuptor c1 = new Cuptor(120, 120.90f);
+        Cuptor c2 = new Cuptor(160, 220.90f);
+        
+        c1.setTimpGatire(180);
+        c1.setGradGatire(180.90f);
+        
+        System.out.println(c1.getTimpGatire());
+        System.out.println(c1.getGradGatire());
+        
+    }
+}
