1f7fdfdbd3a17081ed1b1637c3f2e34fba757a24
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 04:02:25 -0700

    Add files via upload

--- VacantaSimpleFactory/ro/ase/cts/Fabrica/VacantaFactory.java
+++ VacantaSimpleFactory/ro/ase/cts/Fabrica/VacantaFactory.java
@@ -1,20 +1,23 @@
 package ro.ase.cts.Fabrica;
 
+import ro.ase.cts.Vacanta.Compensata;
+import ro.ase.cts.Vacanta.Gratuita;
+import ro.ase.cts.Vacanta.PartialPlatita;
+import ro.ase.cts.Vacanta.Vacanta;
+
 public class VacantaFactory {
     
     public Vacanta getVacanta(TipVacanta tip, String locatie, double pret, int nrPersoane) {
-        
-        switch(tip) {
+
+        return switch(tip) {
             
-            case -> Gratuita = new Gratuita(locatie, pret, nrPersoane);
-            case -> Compensata = new Compensata(locatie, pret, nrPersoane);
-            case -> PartialPlatita = new PartialPlatita(locatie, pret, nrPersoane);
-            default -> throw new IllegalArgumentException("Tip vacanta inexistent: " + tipVacanta);
+            case Gratuita -> new Gratuita(locatie, pret, nrPersoane);
+            case Compensata -> new Compensata(locatie, pret, nrPersoane);
+            case PartialPlatita -> new PartialPlatita(locatie, pret, nrPersoane);
+            default -> throw new IllegalArgumentException("Tip vacanta inexistent: " + tip);
             
-        }
-        
-        return null;
-        
+        };
+
     }
     
 }
