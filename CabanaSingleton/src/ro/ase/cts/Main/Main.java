b612b06aa1e1fb9d9e1bb8628622116a5d9837ad
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 05:14:43 -0700

    Add files via upload

--- CabanaSingleton/src/ro/ase/cts/Main/Main.java
+++ CabanaSingleton/src/ro/ase/cts/Main/Main.java
@@ -5,8 +5,8 @@ import ro.ase.cts.Cabana.Cabana;
 public class Main {
     public static void main(String[] args) {
         
-        Cabana c1 = new Cabana("Locatie1", 5);
-        Cabana c2 = new Cabana("Locatie2", 6);
+        Cabana c1 = new Cabana("Locatie1, Nr camere: ", 5);
+        Cabana c2 = new Cabana("Locatie2, Nr camere: ", 6);
         
         c1.afisareDetaliiCabana();
         c2.afisareDetaliiCabana();
