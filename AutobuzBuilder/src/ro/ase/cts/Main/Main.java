356f5f2e5b1aa7d95bb87aa26b86aebe1dc05d87
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 04:20:18 -0700

    Add files via upload

--- AutobuzBuilder/src/ro/ase/cts/Main/Main.java
+++ AutobuzBuilder/src/ro/ase/cts/Main/Main.java
@@ -1,11 +1,12 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.AutobuzLinie.AutobuzLinie;
+import ro.ase.cts.Autobuz.AutobuzLinie;
+//import ro.ase.cts.AutobuzLinie.AutobuzLinie;
 
 public class Main {
     public static void main(String[] args) {
         
-        AutobuzLinie autobuzLinie = new AutobuzLinie.Builder().setLinie(13).setStatie("Statie1").build();
+        AutobuzLinie autobuzLinie = new AutobuzLinie.Autobuz().setLinie(13).setStatie("Statie1").build();
         
         System.out.println(autobuzLinie.toString());
         
