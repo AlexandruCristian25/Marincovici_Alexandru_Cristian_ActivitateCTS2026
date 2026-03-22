2ff047a0029403ffdad82e9ef2e186e494b64a3e
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 09:54:42 -0700

    Add files via upload

--- FloarePrototype/src/ro/ase/cts/Main/Main.java
+++ FloarePrototype/src/ro/ase/cts/Main/Main.java
@@ -1,23 +1,21 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.RezervareFloare.RezervareFloare;
+import ro.ase.cts.Floare.RezervareFloare;
 
 public class Main {
     public static void main(String[] args) {
         
         RezervareFloare rezervare = new RezervareFloare("Lalea", "Marti");
         RezervareFloare rezervare2 = null;
-        
+
         try {
-            
+
             rezervare2 = rezervare.copiaza();
-            
+
         } catch(CloneNotSupportedException e) {
-            
-            return new RuntimeException(e);
-            
+
         }
-        
+
         rezervare2.setZi("Vineri");
         
         System.out.println(rezervare.toString());
