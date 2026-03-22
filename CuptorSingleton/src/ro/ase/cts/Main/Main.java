9b92e188838420704a2a99cd932f04397317b564
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 05:59:40 -0700

    Add files via upload

--- CuptorSingleton/src/ro/ase/cts/Main/Main.java
+++ CuptorSingleton/src/ro/ase/cts/Main/Main.java
@@ -9,10 +9,10 @@ public class Main {
         Cuptor c2 = new Cuptor(160, 220.90f);
         
         c1.setTimpGatire(180);
-        c1.setGradGatire(180.90f);
+        c1.setGradeGatire(180.90f);
         
         System.out.println(c1.getTimpGatire());
-        System.out.println(c1.getGradGatire());
+        System.out.println(c1.getGradeGatire());
         
     }
 }
