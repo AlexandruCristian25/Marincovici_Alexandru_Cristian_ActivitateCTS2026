bf77ae2bfab6575e0f7576c56d56c4e806915001
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 10:08:17 -0700

    Add files via upload

--- TrenFactoryMethod/src/ro/ase/cts/Main/Main.java
+++ TrenFactoryMethod/src/ro/ase/cts/Main/Main.java
@@ -1,13 +1,17 @@
 package ro.ase.cts.Main;
 
+import ro.ase.cts.Tren.TipTren;
 import ro.ase.cts.Tren.Tren;
+import ro.ase.cts.Tren.TrenFactory;
 
 public class Main {
     public static void main(String[] args) {
+
+        TrenFactory trenFactory = new TrenFactory();
         
-        Tren t1 = TrenFactory.creeazaTren(tipTren.TrenElectric, "Marca1", 12);
-        Tren t2 = TrenFactory.creeazaTren(tipTren.TrenCarbuni, "Marca2", 20);
-        Tren t3 = TrenFactory.creeazaTren(tipTren.TrenMagnetuc, "Marca3", 15);
+        Tren t1 = TrenFactory.creeazaTren(TipTren.TrenElectric, "Marca1 ", 12);
+        Tren t2 = TrenFactory.creeazaTren(TipTren.TrenCarbuni, "Marca2 ", 20);
+        Tren t3 = TrenFactory.creeazaTren(TipTren.TrenMagnetic, "Marca3 ", 15);
         
         t1.afiseazaInfo();
         t2.afiseazaInfo();
