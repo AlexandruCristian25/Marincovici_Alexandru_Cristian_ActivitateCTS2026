4c0e4b6db4d7a621de3bb341de0ad2b04a9b10f5
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 05:58:11 -0700

    Add files via upload

--- ConcertPrototype/src/ro/ase/cts/Main/Main.java
+++ ConcertPrototype/src/ro/ase/cts/Main/Main.java
@@ -1,6 +1,6 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.BiletConcert.BiletConcert;
+import ro.ase.cts.Concert.BiletConcert;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,15 +13,13 @@ public class Main {
             bilet2 = bilet.copiaza();
             
         } catch(CloneNotSupportedException e) {
-            
-            return new RuntimeException(e);
-            
+
         }
         
         bilet2.setNrLoc(56);
         
         System.out.println(bilet.toString());
         System.out.println(bilet2.toString());
-        
+
     }
 }
