9edc1eb27d194d0c6aba176146b213346837ccbb
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 05:46:37 -0700

    Add files via upload

--- ClubSportivFactoryMethod/src/ro/ase/cts/Main/Main.java
+++ ClubSportivFactoryMethod/src/ro/ase/cts/Main/Main.java
@@ -1,13 +1,17 @@
 package ro.ase.cts.Main;
 
 import ro.ase.cts.Jucator.Jucator;
+import ro.ase.cts.Jucator.JucatorFactory;
+import ro.ase.cts.Jucator.TipJucator;
 
 public class Main {
     public static void main(String[] args) {
+
+        JucatorFactory jucatorFactory = new JucatorFactory();
         
-        Jucator j1 = JucatorFactory(tipJucator.Portar, "Alex", 13);
-        Jucator j2 = JucatorFactory(tipJucator.Atacant, "Ionut", 10);
-        Jucator j3 = JucatorFactory(tipJucator.Mijlocas, "Voctor", 17);
+        Jucator j1 = JucatorFactory.afiseazaJucator(TipJucator.Portar, "Alex ", 13);
+        Jucator j2 = JucatorFactory.afiseazaJucator(TipJucator.Atacant, "Ionut ", 10);
+        Jucator j3 = JucatorFactory.afiseazaJucator(TipJucator.Mijlocas, "Victor ", 17);
         
         j1.afiseazaInfo();
         j2.afiseazaInfo();
