1346fe42f9dc2f4953c742c0bb12317d1c1f5c89
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 04:03:43 -0700

    Add files via upload

--- VacantaSimpleFactory/ro/ase/cts/Main/Main.java
+++ VacantaSimpleFactory/ro/ase/cts/Main/Main.java
@@ -1,15 +1,17 @@
 package ro.ase.cts.Main;
 
+import ro.ase.cts.Fabrica.TipVacanta;
+import ro.ase.cts.Fabrica.VacantaFactory;
 import ro.ase.cts.Vacanta.Vacanta;
 
 public class Main {
     public static void main(String[] args) {
         
-        Vacanta vacanta = new Vacanta();
+        VacantaFactory vacantaFactory = new VacantaFactory();
         
-        Vacanta gratuita = vacanta.getVacanta(TipVacanta.Gratuita, "Tokyo", 13500.90, 5);
-        Vacanta compensata = vacanta.getVacanta(TipVacanta.Compensata, "Seoul", 11900.90, 3);
-        Vacanta partialPlatita = vacanta.getVacanta(TipVacanta.PartialPlatita, "Beijing", 17800.90, 7);
+        Vacanta gratuita = vacantaFactory.getVacanta(TipVacanta.Gratuita, "Tokyo", 13500.90, 5);
+        Vacanta compensata = vacantaFactory.getVacanta(TipVacanta.Compensata, "Seoul", 11900.90, 3);
+        Vacanta partialPlatita = vacantaFactory.getVacanta(TipVacanta.PartialPlatita, "Beijing", 17800.90, 7);
         
         gratuita.afiseazaInfo();
         compensata.afiseazaInfo();
