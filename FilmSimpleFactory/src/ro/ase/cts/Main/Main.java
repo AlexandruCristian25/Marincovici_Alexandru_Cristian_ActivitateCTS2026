ad54cb7b5a39239e643ac5853eec6a920dd8b8d7
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 09:52:32 -0700

    Add files via upload

--- FilmSimpleFactory/src/ro/ase/cts/Main/Main.java
+++ FilmSimpleFactory/src/ro/ase/cts/Main/Main.java
@@ -1,21 +1,21 @@
 package ro.ase.cts.Main;
 
 import ro.ase.cts.Film.Film;
+import ro.ase.cts.Film.FilmFactory;
+import ro.ase.cts.Film.TipFilm;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         
-        FilmFactory factory = FilmFactory.getInstance();
+        FilmFactory filmFactoryfactory = new FilmFactory();
         
-        Film filmActiune = factory.getFilm(TipFilm.FilmActiune, 5);
-        Film filmAventura = factory.getFilm(TipFilm.FilmAventura, 15);
-        Film filmComedie = factory.getFilm(TipFilm.FilmComedie, 10);
+        Film f1 = FilmFactory.adaugaFilm(TipFilm.FilmActiune, 5, 25.99);
+        Film f2 = FilmFactory.adaugaFilm(TipFilm.FilmAventura, 15, 45.99);
+        Film f3 = FilmFactory.adaugaFilm(TipFilm.FilmComedie, 10, 30.99);
         
-        System.out.println(filmActiune.toString());
-        System.out.println(filmAventura.toString());
-        System.out.println(filmComedie.toString());
-        
-        FilmFactory factory2 = FilmFactory.getInstance();
+        f1.afiseazaInfo();
+        f2.afiseazaInfo();
+        f3.afiseazaInfo();
         
     }
 }
