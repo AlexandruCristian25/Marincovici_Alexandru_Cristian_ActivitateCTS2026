82795d337e05194fe2fb48ffd9100690d8a3b882
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 05:48:57 -0700

    Add files via upload

--- ConcertBuilder/src/ro/ase/cts/Main/Main.java
+++ ConcertBuilder/src/ro/ase/cts/Main/Main.java
@@ -1,11 +1,11 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.RezervareLoc.RezervareLoc;
+import ro.ase.cts.Concert.RezervareLoc;
 
 public class Main {
     public static void main(String[] args) {
         
-        RezervareLoc rezervare = new RezervareLoc.Builder.setNrLoc("Nr loc: 78").build();
+        RezervareLoc rezervare = new RezervareLoc.RezervareLoc1().Builder().setNrLoc(Integer.parseInt("Nr loc: 78")).build();
         
         System.out.println(rezervare.toString());
         
