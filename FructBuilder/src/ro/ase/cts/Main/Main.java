66a8d09fc566a2edad3d69d21696e76abfb595de
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 09:57:23 -0700

    Add files via upload

--- FructBuilder/src/ro/ase/cts/Main/Main.java
+++ FructBuilder/src/ro/ase/cts/Main/Main.java
@@ -1,12 +1,13 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.FructBuilder.FructBuilder;
+import ro.ase.cts.Fruct.FructBuilder;
+
 
 public class Main {
     
     public static void main(String[] args) {
         
-        FructBuilder fruct = new FructBuilder.Builder.setCuloare("Rosu").build();
+        FructBuilder fruct = new FructBuilder.Builder().setCuloare("Rosu").build();
         
         System.out.println(fruct.toString());
         
