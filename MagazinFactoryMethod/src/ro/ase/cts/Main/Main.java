e66509ccbce0cdff68c25dbc80fad231f1a434ae
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 09:59:45 -0700

    Add files via upload

--- MagazinFactoryMethod/src/ro/ase/cts/Main/Main.java
+++ MagazinFactoryMethod/src/ro/ase/cts/Main/Main.java
@@ -6,12 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Magazin m1 = MagazinFactory.creareMagazin(TipMagazin.ALIMENTAR, "Mega Image", 300);
-        Magazin m2 = MagazinFactory.creareMagazin(TipMagazin.HAINE, "Zara", 500);
-        Magazin m3 = MagazinFactory.creareMagazin(TipMagazin.ELECTRONICE, "Altex", 700);
+        MagazinFactory magazinFactory = new MagazinFactory();
+
+        Magazin m1 = magazinFactory.creareMagazin(TipMagazin.ALIMENTAR, "Mega Image ", 300);
+        Magazin m2 = magazinFactory.creareMagazin(TipMagazin.HAINE, "Zara ", 500);
+        Magazin m3 = magazinFactory.creareMagazin(TipMagazin.ELECTRONICE, "Altex ", 700);
 
         m1.afiseazaDetalii();
         m2.afiseazaDetalii();
         m3.afiseazaDetalii();
+
     }
 }
