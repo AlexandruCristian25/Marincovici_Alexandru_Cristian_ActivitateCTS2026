ce4db303a9d87a376e46ea9deb53c5ea25ac312a
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 04:01:15 -0700

    Add files via upload

--- LaptopFactoryMethod/src/ro/ase/cts/Main/Main.java
+++ LaptopFactoryMethod/src/ro/ase/cts/Main/Main.java
@@ -6,9 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Laptop l1 = LaptopFactory.creareLaptop(TipLaptop.GAMING, "Asus", 32);
-        Laptop l2 = LaptopFactory.creareLaptop(TipLaptop.OFFICE, "Dell", 16);
-        Laptop l3 = LaptopFactory.creareLaptop(TipLaptop.HOME, "HP", 8);
+        LaptopFactory laptopFactory = new LaptopFactory();
+
+        Laptop l1 = laptopFactory.creareLaptop(TipLaptop.GAMING, "Asus", 32);
+        Laptop l2 = laptopFactory.creareLaptop(TipLaptop.OFFICE, "Dell", 16);
+        Laptop l3 = laptopFactory.creareLaptop(TipLaptop.HOME, "HP", 8);
 
         l1.afiseazaDetalii();
         l2.afiseazaDetalii();
