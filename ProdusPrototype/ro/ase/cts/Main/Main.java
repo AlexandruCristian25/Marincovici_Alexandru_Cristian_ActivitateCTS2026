7316c8c5df4e28ab1ea64a89f9996f7102654acc
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 10:05:55 -0700

    Add files via upload

--- ProdusPrototype/ro/ase/cts/Main/Main.java
+++ ProdusPrototype/ro/ase/cts/Main/Main.java
@@ -1,6 +1,6 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.RezervareProdus;
+import ro.ase.cts.Produs.RezervareProdus;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,15 +14,13 @@ public class Main {
             
         } catch(CloneNotSupportedException e) {
             
-            return new Runtime Exception(e);
-            
         }
-        
+
+        rezervare2.setTipProdus("Electronice");
+
+        System.out.println(rezervare.toString());
+        System.out.println(rezervare2.toString());
+
     }
-    
-    rezervare2.setTipProdus("Electronice");
-    
-    System.out.println(rezervare.toString());
-    System.out.println(rezervare2.toString());
     
 }
