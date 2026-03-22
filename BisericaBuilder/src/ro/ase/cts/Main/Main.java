2ebf6dd020ceaa76dd76da47a2e1a0dca9e19f94
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-22 05:04:26 -0700

    Add files via upload

--- BisericaBuilder/src/ro/ase/cts/Main/Main.java
+++ BisericaBuilder/src/ro/ase/cts/Main/Main.java
@@ -1,11 +1,11 @@
-package ro.ase.cts.Main;
+package to.ase.cts.Main;
 
-import ro.ase.cts.Biserica.Biserica;
+import to.ase.cts.Biserica.Biserica;
 
 public class Main {
     public static void main(String[] args) {
         
-        Biserica biserica = new Biserica.Builder().setNume("Nume: Biserica1").setLocatie("Locatie: Locatie1");
+        Biserica biserica = new Biserica.Builder().setNume("Nume: Biserica1").setLocatie("Locatie: Locatie1").build();
         
         System.out.println(biserica.toString());
         
