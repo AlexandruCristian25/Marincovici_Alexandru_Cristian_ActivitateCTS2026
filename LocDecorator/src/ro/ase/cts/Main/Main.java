83797b7b8ae4fdbd23d6bd9aeb7929220534ca2e
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-24 13:25:17 -0700

    Add files via upload

---
+++ LocDecorator/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.Loc;
+
+import ro.ase.cts.Loc.Loc;
+import ro.ase.cts.Loc.Decorator;
+import ro.ase.cts.Loc.LocDecorat;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Loc loc = new Loc(45, 5);
+        loc.afiseazaInfo();
+        
+        Decorator locDecorat = new LocDecorat(loc);
+        
+        locDecorat.aplocaDiscount(7);
+        locDecorat.afiseazaInfo();
+        
+    }
+}
