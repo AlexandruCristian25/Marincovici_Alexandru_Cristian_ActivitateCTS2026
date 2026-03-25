b84b7b25e6552605385003c6c16319061fa18d8d
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-25 23:34:22 +0200

    Add software project creation logic in Main class
    Refactor main method to include software project creation and output.

--- ProiectAdapterClase/src/ro/ase/cts/Main/Main.java
+++ ProiectAdapterClase/src/ro/ase/cts/Main/Main.java
@@ -1,15 +1,34 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.Vacanta.Vacanta;
-import ro.ase.cts.Vacanta.Destinatie;
-import ro.ase.cts.Vacanta.Facade;
+import ro.ase.cts.Proiect.IProiect;
+import ro.ase.cts.Proiect.Proiect;
+import ro.ase.cts.Proiect.CreareSoftware;
+import ro.ase.cts.Proiect.Software;
+import ro.ase.cts.Proiect.SoftwareNou;
 
 public class Main {
+    
     public static void main(String[] args) {
         
-        double pret = 6700.90;
-        
-        System.out.println(Facade.verificarePretPachet(pret));
+        private static void creareSoftware(IProiect proiect) {
+            
+            IProiect proiect = new Proiect();
+            creareSoftware(proiect);
+            
+            System.out.println("Proiect: ");
+            iProiect.info();
+            System.out.println("Software: ");
+            iProiect.creareProiect();
+            
+            System.out.println("");
+            Systrm.out.println("Se doreste utilizarea software-ului nou");
+            System.out.println();
+            Software software = new Software("Software1", 9);
+            CreareSoftware creareSoftware = new CreareSoftware();
+            creareSoftware(creazaSoftware);
+            
+        }
         
     }
+
 }
