b00b2947a6cce7036ed92cb3387056561bf47178
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-25 14:23:48 -0700

    Add files via upload

---
+++ HotelFlyweight/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.Main;
+
+import ro.ase.cts.Hotel.ICamera;
+import ro.ase.cts.Hotel.Camera;
+import ro.ase.cts.Hotel.CameraFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Rezervare rezervareIoana = new Rezervare("Ioana", 5);
+        Rezervare rezervareStefania = new Rezervare("Stefania", 2);
+        Rezervare rezervareMihaela = new Rezervare("Mihaela", 8);
+        
+        CameraFactory factory = new CameraFactory();
+        
+        ICamera camera2 = factory.getCamera(2);
+        camera2.tipareste(rezervareStefania);
+        factory.getCamera(2).tipareste(rezervareIoana);
+        factory.getCamera(3).tipareste(rezervareMihaela);
+        
+    }
+}
