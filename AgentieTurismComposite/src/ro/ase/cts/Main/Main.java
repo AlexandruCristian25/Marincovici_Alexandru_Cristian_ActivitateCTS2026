460d31286135282397c18fc2a8592f0d9a08eb02
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-27 14:08:12 -0700

    Add files via upload

---
+++ AgentieTurismComposite/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,46 @@
+package ro.ase.cts.Main;
+
+import ro.ase.cts.Agentie.Categorie;
+import ro.ase.cts.Agentie.Optiune;
+import ro.ase.cts.Agentie.Element;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Optiune cFisier = new Categorie("Fisier");
+        Optiune cCamere = new Categorie("Camere");
+        Optiune cOferta = new Categorie("Oferta");
+        
+        Optiune eOpen = new Element("Open");
+        Optiune eSave = new Element("Save");
+        Optiune eVizualizare = new Element("Vizualizare");
+        Optiune eEditare = new Element("Editare");
+        Optiune eOfertaPaste = new Element("Oferta Paste");
+        Optiune eCreazaOferta = new Element("Creare oferta");
+        
+        try {
+            
+            cCamere.adaugaNod(eVizualizare);
+            cCamere.adaugaNod(eEditare);
+            
+            cOferta.adaugaNod(eOfertaPaste);
+            cOferta.adaugaNod(eCreazaOferta);
+            
+            cFile.adaugaNod(eOpen);
+            cFile.adaugaNod(eSave);
+            cFile.adaugaNod(cCamere);
+            cFile.adaugaNod(cOferta);
+            
+            cFile.stergeNod(eSave);
+            cOferte.adaugaNod(eSave);
+            
+            cFile.descriere();
+            
+        } catch(Exception e) {
+            
+            e.printStackTrace();
+            
+        }
+        
+    }
+}
