e2703c6950b2b96db37439192c6ebd7c65d8c278
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-29 13:42:59 -0700

    Add files via upload

---
+++ MasinaFlyweight/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.Masina;
+
+import ro.ase.cts.Masina.MasinaFactory;
+import ro.ase.cts.Masina.IMasina;
+import ro.ase.cts.Masina.Rezervare;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Rezervare rezervareVasile = new Rezervare("Vasile", 3700);
+        Rezervare rezervareAndrei = new Rezervare("Andrei", 4200);
+        Rezervare rezervareMihai = new Rezervare("Mihai", 4000);
+        
+        MasinaFactory factory = new MasinaFactory();
+        
+        IMasina masina2 = factory.getMasina(3200);
+        masina2.afiseazaInfo(rezervareVasile);
+        factory.getMasina(3900).afiseazaInfo(rezervareAndrei);
+        factory.getMasina(3500).afiseazaInfo(rezervareMihai);
+        
+    }
+}
