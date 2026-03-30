a97b90263fcd7dbcc8c55ddac2d0510d273a1f10
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-03-30 13:18:36 -0700

    Add files via upload

--- AplicatieMobilaAdapterClase/src/ro/ase/cts/Main/Main.java
+++ AplicatieMobilaAdapterClase/src/ro/ase/cts/Main/Main.java
@@ -6,18 +6,21 @@ import ro.ase.cts.Aplicatie.AplicatieMobila;
 import ro.ase.cts.Aplicatie.Aplicatie;
 
 public class Main {
+
+    public static void afiseazaAplicatie(IAplicatieMobila iAplicatie) {
+        System.out.println("Tip aplicatie: ");
+        iAplicatie.descriere();
+        System.out.println("Se creeaza software-ul");
+        iAplicatie.creeazaSoftware();
+    }
+
     public static void main(String[] args) {
-        
-        IAplicatie iAplicatie = new IAplicatie();
-        
-        private static afiseazaAplicatie(IAplicatie iAplicatie) {
-            
-            System.out.println("Tip aplicatie: ");
-            iAplicatie.descriere();
-            System.out.println("Se creeaza software-ul");
-            iAplicatie.creeazaSoftware();
-            
-        }
-        
+
+        IAplicatieMobila software = new Software();
+        afiseazaAplicatie(software);
+
+        Aplicatie aplicatie = new Aplicatie("WhatsApp", 150);
+        IAplicatieMobila aplicatieMobila = new AplicatieMobila(aplicatie);
+        afiseazaAplicatie(aplicatieMobila);
     }
 }
