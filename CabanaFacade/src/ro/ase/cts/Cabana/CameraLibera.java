212476539ef3d6a47d09cad4903f36880320c614
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 22:47:54 +0300

    Refactor CameraLibera to initialize listaCamereLibere

--- CabanaFacade/src/ro/ase/cts/Cabana/CameraLibera.java
+++ CabanaFacade/src/ro/ase/cts/Cabana/CameraLibera.java
@@ -1,14 +1,20 @@
 package ro.ase.cts.Cabana;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CameraLibera {
-    
-    listaCamereLibere = new ArrayList<Integer>();
-    for(int i = 0; i < 12; i++) {
-        
-        listaCamereLibere.add(i + 1);
-        
+
+    private List<Integer> listaCamereLibere;
+
+    public CameraLibera() {
+        listaCamereLibere = new ArrayList<Integer>();
+        for (int i = 0; i < 12; i++) {
+            listaCamereLibere.add(i + 1);
+        }
     }
-    
-    public boolean verificaCamereLibere.contains(nrCamera);
-    
+
+    public boolean verificaCamereLibere(int nrCamera) {
+        return listaCamereLibere.contains(nrCamera);
+    }
 }
