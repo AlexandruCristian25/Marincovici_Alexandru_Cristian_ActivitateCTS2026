ede6dd0155ab260b45b26aab969eb1850a6ac051
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 22:53:42 +0300

    Remove unnecessary whitespace and comments

--- CabanaTemplate/src/ro/ase/cts/Cabana/PachetCazare.java
+++ CabanaTemplate/src/ro/ase/cts/Cabana/PachetCazare.java
@@ -3,16 +3,12 @@ package ro.ase.cts.Cabana;
 public class PachetCazare extends Cabana {
     
     public PachetCazare(String locatie) {
-        
         super(locatie);
-        
     }
     
     @Override
     public void pachetCazare(String locatie) {
-        
         System.out.println("S-a gasit cabana pentru pachet in locatie " + locatie);
-        
     }
     
     @Override
@@ -22,9 +18,6 @@ public class PachetCazare extends Cabana {
     
     @Override
     public void achitarePachet(String locatie) {
-        
         System.out.println("Locatia cabanei este " + locatie);
-        
     }
-    
 }
