30c7d3373ed91efda17b874a3c54f78889c76115
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 23:02:11 +0300

    Add files via upload

---
+++ CalculatorComposite/src/ro/ase/cts/Calculator/Optiune.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.Calculator;
+
+public interface Optiune {
+    
+    void stergeNod(Optiune optiune) throws Exception;
+    void adaugaNod(Optiune optiune) throws Exception;
+    Optiune getNod(int index) throws Exception;
+    void descriere();
+    
+}
