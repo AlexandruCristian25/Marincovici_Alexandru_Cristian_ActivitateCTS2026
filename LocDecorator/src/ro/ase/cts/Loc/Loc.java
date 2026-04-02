0553139abd83cf64d67c63ce8b9285eb61d484e7
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 23:56:24 +0300

    Add files via upload

--- LocDecorator/src/ro/ase/cts/Loc/Loc.java
+++ LocDecorator/src/ro/ase/cts/Loc/Loc.java
@@ -6,41 +6,30 @@ public class Loc implements ILoc {
     private int rand;
     
     public Loc(int nrLoc, int rand) {
-        
         super();
         this.nrLoc = nrLoc;
         this.rand = rand;
-        
     }
     
+    @Override
     public void afiseazaInfo() {
-        
         System.out.println("Locul are " + nrLoc + " si se afla pe randul " + rand);
-        
     }
     
     public int getNrLoc() {
-        
         return nrLoc;
-        
     }
     
     public void setNrLoc(int nrLoc) {
-        
         this.nrLoc = nrLoc;
-        
     }
     
     public int getRand() {
-        
         return rand;
-        
     }
     
     public void setRand(int rand) {
-        
         this.rand = rand;
-        
     }
     
 }
--- LocDecorator/src/ro/ase/cts/Loc/LocDecorat.java
+++ LocDecorator/src/ro/ase/cts/Loc/LocDecorat.java
@@ -1,20 +1,14 @@
 package ro.ase.cts.Loc;
 
-import ro.ase.cts.Loc.Loc;
-
 public class LocDecorat extends Decorator {
     
-    public LocDecorator(Loc loc) {
-        
+    public LocDecorat(Loc loc) {
         super(loc);
-        
     }
     
     @Override
     public void aplicaDiscount(int procent) {
-        
         super.loc.setRand(super.loc.getRand() * (100 - procent) / 100);
-        
     }
     
 }
