52f3bb0ab4082e50cec21fe5663438df364c2c16
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 22:49:47 +0300

    Refactor Menajera class to fix camera list initialization

--- CabanaFacade/src/ro/ase/cts/Cabana/Menajera.java
+++ CabanaFacade/src/ro/ase/cts/Cabana/Menajera.java
@@ -4,36 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menajera {
-    
+
     private List<Boolean> listaCamere;
-    
+
     public Menajera() {
-        
-        listaCamera = new ArrayList<Boolean>();
-        for(int i = 0; i < 8; i++) {
-            
+
+        listaCamere = new ArrayList<Boolean>();
+
+        for (int i = 0; i < 8; i++) {
             listaCamere.add(true);
-            
         }
-        
-        for(int i = 8; i < 14; i++) {
-            
+
+        for (int i = 8; i < 14; i++) {
             listaCamere.add(false);
-            
         }
-        
     }
-    
+
     public boolean cameraLibera(int nr) {
-        
-        return listaCamereLibere.get(nr);
-        
+        return listaCamere.get(nr - 1);
     }
-    
+
     public boolean esteCurata(int nrCamera) {
-        
-        return listaCamereLibere.get(nrCamera);
-        
+        return listaCamere.get(nrCamera - 1);
     }
-    
 }
