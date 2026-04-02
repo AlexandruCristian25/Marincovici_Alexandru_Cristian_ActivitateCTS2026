4917775fc38a2efc09948c799a64c70a301661a2
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 22:52:45 +0300

    Refactor Cabana constructor and method names

--- CabanaTemplate/src/ro/ase/cts/Cabana/Cabana.java
+++ CabanaTemplate/src/ro/ase/cts/Cabana/Cabana.java
@@ -4,24 +4,20 @@ public abstract class Cabana {
     
     protected String locatie;
     
-    public Cabana(String locatie, boolean areEtaj) {
-        
+    public Cabana(String locatie) {
         super();
         this.locatie = locatie;
-        this.areEtaj = areEtaj;
-        
     }
     
-    public abstract void cautareCazare(String locatie);
+    public abstract void pachetCazare(String locatie);
     public abstract void rezervareCabana(String locatie);
     public abstract void achitarePachet(String locatie);
     
     public final void vindePachet() {
         
-        cautareCazare(locatie);
+        pachetCazare(locatie);
         rezervareCabana(locatie);
         achitarePachet(locatie);
         
     }
-    
 }
