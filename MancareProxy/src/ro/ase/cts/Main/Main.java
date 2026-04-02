64d12397dabeaa16ced7f15689b4b0a2aca4569f
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-03 00:01:50 +0300

    Add files via upload

--- MancareProxy/src/ro/ase/cts/Main/Main.java
+++ MancareProxy/src/ro/ase/cts/Main/Main.java
@@ -1,5 +1,6 @@
 package ro.ase.cts.Main;
 
+import ro.ase.cts.Mancare.FelMancare;
 import ro.ase.cts.Mancare.IMancare;
 import ro.ase.cts.Mancare.Mancare;
 import ro.ase.cts.Mancare.MancareProxy;
@@ -8,10 +9,13 @@ public class Main {
     public static void main(String[] args) {
         
         Mancare mancare = new Mancare("Tocanita de cartofi", 17.99);
-        IMancare iMancare = new IMancare(mancare);
-        iMancare.gatesteMancare();
         
-        MancareProxy iMancareProxy = new MancareProxy(mancare);
-        iMancareProxy.gatesteMancare();
+        IMancare fel = new FelMancare(mancare);
+        fel.gatesteMancare();
+        
+        IMancare proxy = new MancareProxy(fel);
+        proxy.gatesteMancare();
         
     }
+    
+}
