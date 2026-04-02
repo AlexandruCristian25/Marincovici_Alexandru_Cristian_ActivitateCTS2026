783bc382877b70f2092aa3daa1dbd12d1f29b3f6
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 22:37:42 +0300

    Add files via upload

---
+++ LaptopStrategy/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.Main;
+
+import ro.ase.cts.Laptop.LaptopGaming;
+import ro.ase.cts.Laptop.LaptopOffice;
+import ro.ase.cts.Laptop.LaptopHome;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Programator programator = new Programator("Mihai");
+        
+        programator.setTipLaptop(new LaptopGaming());
+        programator.utilizeaza(16);
+        
+        programator.setTipLaptop(new LaptopOffice());
+        programator.utilizeaza(8);
+        
+    }
+}
