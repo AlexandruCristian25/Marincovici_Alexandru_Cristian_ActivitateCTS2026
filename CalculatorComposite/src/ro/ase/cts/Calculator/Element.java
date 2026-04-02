c757f5cbf889b20eb3c5a67366a35c6ec387eed1
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 23:05:51 +0300

    Add files via upload

--- CalculatorComposite/src/ro/ase/cts/Calculator/Element.java
+++ CalculatorComposite/src/ro/ase/cts/Calculator/Element.java
@@ -5,37 +5,26 @@ public class Element implements Optiune {
     private String nume;
     
     public Element(String nume) {
-        
         this.nume = nume;
-        
     }
     
     @Override
     public void stergeNod(Optiune optiune) throws Exception {
-        
         throw new Exception("Nu exista!");
-        
     }
     
     @Override
     public void adaugaNod(Optiune optiune) throws Exception {
-        
         throw new Exception("Nu exista!");
-        
     } 
     
     @Override
     public Optiune getNod(int index) throws Exception {
-        
         throw new Exception("Nu exista!");
-        
     }
     
     @Override
     public void descriere() {
-        
         System.out.println("Element: " + nume);
-        
     }
-    
 }
