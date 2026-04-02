010c324d13d15a9797a0785181240a25257bf63e
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 22:49:13 +0300

    Refactor camera availability checks in Facade

--- CabanaFacade/src/ro/ase/cts/Cabana/Facade.java
+++ CabanaFacade/src/ro/ase/cts/Cabana/Facade.java
@@ -1,56 +1,50 @@
 package ro.ase.cts.Cabana;
 
-import ro.ase.cts.Cabana.CameraLibera;
-import ro.ase.cts.Cabana.Menajera;
-
 public class Facade {
-    
+
     public static boolean verificaCamera(int nrCamera) {
-        
-        CameraLibera camera = new CameraLibera();
-        if(libera.verificaCamereLibere(nrCamera)) {
-            
-            Menajera menajeta = new Menajera();
-            if(menajera.cameraLibera(nrCamera) && menajera.esteCurata(nrCamera)) {
-                
-                return true;
-                
-            } else {
-                
-                return false;
-                
-            }
-            
-        } else {
-            
-            return false;
-            
-        }
-        
-    }
-    
-    public static String verificaDisponibilitateCamera(int nrCamera) {
-        
+
         CameraLibera libera = new CameraLibera();
-        if(libera.verificaCamereLibere(nrCamera)) {
-            
+        if (libera.verificaCamereLibere(nrCamera)) {
+
             Menajera menajera = new Menajera();
-            if(menajera.cameraLibera(nrCamera) && menajera.esteCurata(nrCamera)) {
-                
-                return "Camera pregatita";
-                
+            if (menajera.cameraLibera(nrCamera) && menajera.esteCurata(nrCamera)) {
+
+                return true;
+
             } else {
-                
-                return "Camera in curs de pregatire"
-                
+
+                return false;
+
             }
-            
+
         } else {
-            
-            return "Camera in curs de pregatire";
-            
+
+            return false;
+
         }
-        
     }
-    
+
+    public static String verificaDisponibilitateCamera(int nrCamera) {
+
+        CameraLibera libera = new CameraLibera();
+        if (libera.verificaCamereLibere(nrCamera)) {
+
+            Menajera menajera = new Menajera();
+            if (menajera.cameraLibera(nrCamera) && menajera.esteCurata(nrCamera)) {
+
+                return "Camera pregatita";
+
+            } else {
+
+                return "Camera in curs de pregatire";
+
+            }
+
+        } else {
+
+            return "Camera aproape finalizata";
+
+        }
+    }
 }
