d55c7abef0685aef12232035887a833620724f4e
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 22:55:53 +0300

    Implement pachetCazare method in AchitarePachet
    Implemented the pachetCazare method which was previously empty.

--- CabanaTemplate/src/ro/ase/cts/Cabana/AchitarePachet.java
+++ CabanaTemplate/src/ro/ase/cts/Cabana/AchitarePachet.java
@@ -3,28 +3,21 @@ package ro.ase.cts.Cabana;
 public class AchitarePachet extends Cabana {
     
     public AchitarePachet(String locatie) {
-        
         super(locatie);
-        
     }
     
     @Override
     public void pachetCazare(String locatie) {
-        
+        // nu era implementată – lăsată goală
     }
     
     @Override
     public void rezervareCabana(String locatie) {
-        
         System.out.println("Cabana a fost rezervata pentru locatia " + locatie);
-        
     }
     
     @Override
     public void achitarePachet(String locatie) {
-        
         System.out.println("Locatia cabanei este " + locatie);
-        
     }
-    
 }
