860c2d6558c6a04a8c66dcba5b5fcc6558de49ae
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-02 22:50:49 +0300

    Change camera number from 45 to 5 in Main.java

--- CabanaFacade/src/ro/ase/cts/Main/Main.java
+++ CabanaFacade/src/ro/ase/cts/Main/Main.java
@@ -1,15 +1,13 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.Cabana.CameraLibera;
-import ro.ase.cts.Cabana.Menajera;
 import ro.ase.cts.Cabana.Facade;
 
 public class Main {
     public static void main(String[] args) {
-        
-        int nrCamera = 45;
-        
+
+        int nrCamera = 5;
+
         System.out.println(Facade.verificaDisponibilitateCamera(nrCamera));
-        
+
     }
 }
