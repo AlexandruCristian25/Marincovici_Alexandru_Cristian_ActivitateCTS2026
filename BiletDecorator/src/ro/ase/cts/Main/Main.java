db4a3e19dc727dd0b9be2f9cff709b99d194fd6c
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-03 11:08:58 +0300

    Add files via upload

--- BiletDecorator/src/ro/ase/cts/Main/Main.java
+++ BiletDecorator/src/ro/ase/cts/Main/Main.java
@@ -1,20 +1,21 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.Factura.Factura;
-import ro.ase.cts.Factura.FacturaDecorator;
-import ro.ase.cts.Factura.Decorator;
+import ro.ase.cts.Bilet.Bilet;
+import ro.ase.cts.Bilet.IBilet;
+import ro.ase.cts.Bilet.BiletDecorat;
 
 public class Main {
+
     public static void main(String[] args) {
-        
-        Bilet bilet = new Bilet(4, "Ionescu");
-        
-        bilet.afiseazaInfo();
-        
-        Decorator biletDecorator = new BiletDecorator(bilet);
-        
-        biletDecorator.aplicareDiscount(30);
-        biletDecorator.afiseazaInfo();
+
+        IBilet bilet = new Bilet("Ion", 50);
+        bilet.afisareBilet();
+
+        System.out.println();
+
+        IBilet biletDecorat = new BiletDecorat(bilet);
+        biletDecorat.afisareBilet();
         
     }
+    
 }
