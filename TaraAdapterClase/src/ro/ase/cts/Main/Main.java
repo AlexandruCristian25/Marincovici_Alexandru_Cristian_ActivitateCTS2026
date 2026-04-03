a52986144a855f41ff391eb894a02d21f6ccd6fb
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-03 10:45:39 +0300

    Add files via upload

--- TaraAdapterClase/src/ro/ase/cts/Main/Main.java
+++ TaraAdapterClase/src/ro/ase/cts/Main/Main.java
@@ -1,29 +1,31 @@
 package ro.ase.cts.Main;
 
 import ro.ase.cts.Tara.ITara;
-import ro.ase.cts.Tara.Cazare;
 import ro.ase.cts.Tara.Tara;
+import ro.ase.cts.Tara.AdapterTara;
+import ro.ase.cts.Tara.TaraExterna;
 
 public class Main {
+
+    public static void afiseazaTara(ITara tara) {
+        System.out.println("Informatii tara:");
+        tara.afiseazaTara();
+        System.out.println("Populatie:");
+        tara.afiseazaPopulatie();
+    }
+
     public static void main(String[] args) {
-        
-        private static void afisareRezervare(ITara iTara) {
-            
-            System.out.println("Pentru client: ");
-            iTara.afisareInfo();
-            System.out.println("Pentru hotel: ");
-            iTara.rezervaPachet();
-            
-        }
-        
-        ITara cazare = new Cazare();
-        afisareRezervare(cazare);
-        
-        System.out.println("Se doreste cazarea camerei catre hotel");
-        
-        System.put.println("Cazare camera hotel");
-        Tara tara = new Tara("Coreea de Sud", 20);
-        System.out.println(tara.toString());
+
+        ITara tara = new Tara("Romania", 19000000);
+        afiseazaTara(tara);
+
+        System.out.println("Se foloseste tara externa: ");
+
+        TaraExterna taraExterna = new TaraExterna("Germania", 83000000);
+        ITara adapter = new AdapterTara(taraExterna);
+
+        afiseazaTara(adapter);
         
     }
+    
 }
