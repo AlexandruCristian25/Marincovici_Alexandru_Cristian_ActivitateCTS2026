5d5468872248af3c21058439db6d172d1cecface
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-03 10:31:49 +0300

    Add files via upload

--- ProdusDecorator/src/ro/ase/cts/Produs/ProdusDecorat.java
+++ ProdusDecorator/src/ro/ase/cts/Produs/ProdusDecorat.java
@@ -1,20 +1,14 @@
-package ro.ase.cts.Pdodus;
-
-import ro.ase.cts.Produs.Produs;
+package ro.ase.cts.Produs;
 
 public class ProdusDecorat extends Decorator {
     
     public ProdusDecorat(Produs produs) {
-        
         super(produs);
-        
     }
     
     @Override
     public void aplicaDiscount(int procent) {
-        
         super.produs.setPret(super.produs.getPret() * (100 - procent) / 100);
-        
     }
     
 }
