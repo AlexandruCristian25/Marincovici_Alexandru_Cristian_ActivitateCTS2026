c2fc9a3d86101b328a1af7c5e5f68f4ecddf4184
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-03 10:38:21 +0300

    Add files via upload

--- ProiectAdapterClase/src/ro/ase/cts/Main/Main.java
+++ ProiectAdapterClase/src/ro/ase/cts/Main/Main.java
@@ -7,28 +7,26 @@ import ro.ase.cts.Proiect.Software;
 import ro.ase.cts.Proiect.SoftwareNou;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        
-        private static void creareSoftware(IProiect proiect) {
-            
-            IProiect proiect = new Proiect();
-            creareSoftware(proiect);
-            
-            System.out.println("Proiect: ");
-            iProiect.info();
-            System.out.println("Software: ");
-            iProiect.creareProiect();
-            
-            System.out.println("");
-            Systrm.out.println("Se doreste utilizarea software-ului nou");
-            System.out.println();
-            Software software = new Software("Software1", 9);
-            CreareSoftware creareSoftware = new CreareSoftware();
-            creareSoftware(creazaSoftware);
-            
-        }
-        
+
+    public static void creareSoftware(IProiect proiect) {
+        System.out.println("Proiect: ");
+        proiect.info();
+        System.out.println("Software: ");
+        proiect.adaugaProiect();
     }
 
+    public static void main(String[] args) {
+
+        IProiect proiect = new Proiect();
+        creareSoftware(proiect);
+
+        System.out.println("Se doreste utilizarea software-ului nou");
+
+        Software software = new Software("Software1", 9);
+        IProiect adapter = new SoftwareNou(software);
+
+        creareSoftware(adapter);
+        
+    }
+    
 }
