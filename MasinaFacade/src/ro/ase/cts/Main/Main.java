6343d004d145f01cd92fda813dcf567351421d3e
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-03 10:11:23 +0300

    Add files via upload

--- MasinaFacade/src/ro/ase/cts/Main/Main.java
+++ MasinaFacade/src/ro/ase/cts/Main/Main.java
@@ -8,7 +8,7 @@ public class Main {
     
     public static void main(String[] args) {
         
-        String tipMasina = "Noua";
+        String tipMasina = "1";
         
         System.out.println(Facade.verificareMasina(tipMasina));
         
