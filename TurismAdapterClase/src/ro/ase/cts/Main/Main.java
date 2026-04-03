56c1417c5eb0cf7fa305abb7d25de8355da0b904
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-03 10:52:43 +0300

    Add files via upload

--- TurismAdapterClase/src/ro/ase/cts/Main/Main.java
+++ TurismAdapterClase/src/ro/ase/cts/Main/Main.java
@@ -1,32 +1,35 @@
 package ro.ase.cts.Main;
 
-import ro.ase.cts.Turism.PachetTurism;
+import ro.ase.cts.Turism.PachetTuristic;
 import ro.ase.cts.Turism.PachetCazare;
 import ro.ase.cts.Turism.Autocar;
-import ro.ase.cts.Turism.PachetAutocar;
-import ro.ase.cts.Turism.PachetAutocarInchitiat;
+import ro.ase.cts.Turism.PachetAutocarInchiriat;
 
 public class Main {
-    public static void main(String[] args) {
-        
-        PachetTuristic pachetCazare = new PachetTuristic();
-        
-        afisareRezervare(pachetCazare);1
-        
-        private static void afiseazaRezervare(PachetTuristic pachetTuristic) {
-            
-            System.out.println("Pentru turist: ");
-            pachetTuristic.descriere();
-            
-            System.out.println("Pentru agentie: ");
-            pachetTuristic.rezervaPachet();
-            
-        }
-        
-        System.out.println();
-        System.out.println("Utilizare autocar");
-        Autocar autocar = new Autocar("Mercedes", 52);
-        AutocarInchiriat autocarInchiriat = new AutocarInchiriat(autocar);
+
+    public static void afiseazaRezervare(PachetTuristic pachetTuristic) {
+
+        System.out.println("Pentru turist: ");
+        pachetTuristic.descriere();
+
+        System.out.println("Pentru agentie: ");
+        pachetTuristic.rezervarePachet();
         
     }
+
+    public static void main(String[] args) {
+
+        PachetTuristic pachetCazare = new PachetCazare();
+        afiseazaRezervare(pachetCazare);
+
+        System.out.println();
+        System.out.println("Utilizare autocar");
+
+        Autocar autocar = new Autocar("Mercedes", 52);
+        PachetTuristic pachetAutocar = new PachetAutocarInchiriat(autocar);
+
+        afiseazaRezervare(pachetAutocar);
+        
+    }
+    
 }
