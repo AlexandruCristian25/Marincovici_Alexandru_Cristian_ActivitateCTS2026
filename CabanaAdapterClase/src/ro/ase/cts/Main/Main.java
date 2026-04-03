46b4735d1cb1cd45b4c2bb2ce0841ffee73cb9bc
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-03 11:21:19 +0300

    Add files via upload

--- CabanaAdapterClase/src/ro/ase/cts/Main/Main.java
+++ CabanaAdapterClase/src/ro/ase/cts/Main/Main.java
@@ -1,16 +1,20 @@
 package ro.ase.cts.Main;
 
+import ro.ase.cts.Cabana.CazareCabana;
+import ro.ase.cts.Cabana.ICabana;
+import ro.ase.cts.Cabana.Masina;
+import ro.ase.cts.Cabana.MasinaInchiriata;
+
 public class Main {
+
+    public static void printeazaRezervare(ICabana iCabana) {
+        System.out.println("Pentru client: ");
+        iCabana.info();
+        System.out.println("Pentru sofer: ");
+        iCabana.rezervaCabana();
+    }
+
     public static void main(String[] args) {
-        
-        private static void printeazaRezervare(ICabana iCabana) {
-            
-            System.out.println("Pentru client: ");
-            iCabana.info();
-            System.out.println("Pentru sofer: ");
-            iCabana.rezervareCabana();
-            
-        }
         
         ICabana cazareCabana = new CazareCabana();
         printeazaRezervare(cazareCabana);
@@ -20,7 +24,10 @@ public class Main {
         System.out.println("Masina inchiriata");
         Masina masina = new Masina("Audi", "Popescu");
         MasinaInchiriata masinaInchiriata = new MasinaInchiriata(masina);
+        
         System.out.println(masina.toString());
+        
+        printeazaRezervare(masinaInchiriata);
         
     }
 }
