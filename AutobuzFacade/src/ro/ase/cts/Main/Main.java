1b1089973fee5c82b68265bb8837912a13c4ca7e
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-18 22:26:36 +0300

    Add files via upload

---
+++ AutobuzFacade/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.Main;
+
+import ro.ase.cts.Autobuz.Facade;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int nrUsi = 3;
+
+        System.out.println(Facade.verificaExistentaButoane(nrUsi));
+        
+    }
+    
+}
