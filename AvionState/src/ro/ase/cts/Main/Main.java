f22b7ffbe0d3314211bd19ca7f3c5ccb1755a97c
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-04-25 16:17:19 +0300

    Add files via upload

---
+++ AvionState/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,14 @@
+package ro.ase.cts.Main;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        LocAvion locAvion = new LocAvion(23);
+        locAvion.ocupaLoc();
+        locAvion.rezervareLoc();
+        locAvion.rezervareLoc();
+        locAvion.ocupaLoc();
+        
+    }
+    
+}
