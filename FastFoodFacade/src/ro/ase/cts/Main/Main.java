8ae84b240e38ee2b3518162b881abca6f5fd63f4
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-05-04 21:01:01 +0300

    Add files via upload

---
+++ FastFoodFacade/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,13 @@
+package ro.ase.cts.Main;
+
+import ro.ase.cts.FastFood.Facade;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int nrMasa = 7;
+
+        System.out.println(Facade.verificareDisponibilitateMasa(nrMasa));
+    }
+}
