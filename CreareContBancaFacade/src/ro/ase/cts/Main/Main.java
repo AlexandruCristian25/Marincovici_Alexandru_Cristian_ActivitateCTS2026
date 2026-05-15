7e5ed8b0b6eb75a350a7ee6fe7370ee1eac4cc3d
Marincovici Alexandru Cristian <75697428+AlexandruCristian25@users.noreply.github.com>
2026-05-15 23:58:35 +0300

    Add files via upload

---
+++ CreareContBancaFacade/src/ro/ase/cts/Main/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String numeClient = "Client1";
+
+        System.out.println(Facade.verificareDisponibilitateClient(numeClient));
+
+    }
+}
